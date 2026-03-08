4082806777dad492a5858f45ee878ae551992134
rohan Yadav <r6461719@gmail.com>
2026-03-08 10:16:01 +0545

    changes

--- script.py
+++ script.py
@@ -4,13 +4,31 @@ import datetime
 import webbrowser
 import os
 import pywhatkit
+import random
 
 engine = pyttsx3.init()
 listener = sr.Recognizer()
 
+responses = [
+    "I'm ready, sir.",
+    "How can I help you?",
+    "Yes, tell me your command.",
+    "At your service!"
+]
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+
+def greet_user():
+    hour = datetime.datetime.now().hour
+    if hour < 12:
+        talk("Good morning!")
+    elif hour < 18:
+        talk("Good afternoon!")
+    else:
+        talk("Good evening!")
+    talk(random.choice(responses))
 
 def take_command():
     try:
@@ -25,6 +43,9 @@ def take_command():
 
 def run_assistant():
     command = take_command()
+    if not command:
+        return
+
     print(f"Command: {command}")
 
     if "open youtube" in command:
@@ -40,11 +61,18 @@ def run_assistant():
     elif "time" in command:
         now = datetime.datetime.now().strftime("%H:%M")
         talk(f"The time is {now}")
-    elif "exit" in command:
-        talk("Goodbye!")
+    elif "date" in command:
+        today = datetime.datetime.now().strftime("%B %d, %Y")
+        talk(f"Today's date is {today}")
+    elif "who are you" in command or "your name" in command:
+        talk("I am your advanced assistant, at your service!")
+    elif "exit" in command or "quit" in command:
+        talk("Goodbye! Have a nice day.")
         exit()
     else:
-        talk("I didn't understand that command")
+        talk("I didn't understand that. Can you repeat?")
+
+greet_user()
 
 while True:
     run_assistant()
